e3818a6808ef3c0ff00501865eb15819391bf718
Yulia Tsareva <tsarovayulia@gmail.com>
2022-04-11 23:51:13 +0300

    task storage interface

--- libs/task_storage/include/task_storage.h
+++ libs/task_storage/include/task_storage.h
@@ -4,6 +4,7 @@
 #include <libs/common/include/slot.h>
 #include <libs/state/include/state.h>
 
+#include <chrono>
 #include <optional>
 
 
@@ -16,17 +17,44 @@ public:
     TaskStorage() = default;
     virtual ~TaskStorage() = default;
 
-    virtual Result<TaskId> addTask(const TaskParameters& taskParameters) noexcept = 0;
+    virtual Result<CreatedJobs> addTask(const TaskParameters& taskParameters) noexcept = 0;
     virtual Result<PlanId> startScheduling(
-        const std::string& backendId, const std::chrono::seconds& schedulingInterval) noexcept = 0;
+        const std::string& backendId,
+        const std::chrono::seconds& schedulingInterval) noexcept = 0;
     virtual Result<State> getCurrentState() noexcept = 0;
     virtual Result<void> commitPlanChange(const StateChange& state, const PlanId planId) noexcept = 0;
     virtual Result<std::vector<AllocationPendingVmInfo>> getVmsToAllocate(
         const size_t maxVmAllocationCount) noexcept = 0;
-    virtual Result<void> setVmStatus(const VmId id, const VmStatus status) noexcept = 0;
-    virtual Result<void> saveVmAllocationResult(const VmId id, const AllocatedVmInfo& allocatedVmInfo) noexcept = 0;
     virtual Result<std::vector<TerminationPendingVmInfo>> getVmsToTerminate(
         const size_t maxVmATerminationCount) noexcept = 0;
+    virtual Result<void> rollbackUnallocatedVmsState(
+        const std::vector<VmId>& vmIds,
+        const size_t vmRestartAttemptCount,
+        const size_t jobRestartAttemptCount) noexcept = 0;
+    virtual Result<void> returnUnterminatedVms(const std::vector<VmId>& vmIds) noexcept = 0;
+    virtual Result<void> saveVmAllocationResult(const VmId id, const AllocatedVmInfo& allocatedVmInfo) noexcept = 0;
+    virtual Result<void> saveVmTerminationResult(const VmId id) noexcept = 0;
+    virtual Result<JobStates> getJobStates(const TaskId id) noexcept = 0;
+    virtual Result<void> cancelTask(const TaskId taskId) noexcept = 0;
+    virtual Result<void> restartStaleAllocatingVms(
+        const std::chrono::seconds& allocationTimeLimit,
+        const size_t vmRestartAttemptCount) noexcept = 0;
+    virtual Result<void> terminateStaleAllocatingVms(
+        const std::chrono::seconds& allocationTimeLimit,
+        const size_t vmRestartAttemptCount,
+        const size_t jobRestartAttemptCount) noexcept = 0;
+    virtual Result<void> terminateVmsWithInactiveAgents(
+        const std::chrono::seconds& agentInactivityTimeLimit,
+        const size_t jobRestartAttemptCount) noexcept = 0;
+    virtual Result<void> terminateVmsWithoutAgents(
+        const std::chrono::seconds& agentStartupTimeLimit,
+        const size_t jobRestartAttemptCount) noexcept = 0;
+    virtual Result<std::vector<AssignedJob>> getAssignedJobs(
+        const VmId vmId) noexcept = 0;
+    virtual Result<JobToLaunch> getJobToLaunch(
+        const VmId vmId, const JobId jobId) noexcept = 0;
+    virtual Result<void> updateJobState(
+        const VmId vmId, const JobId jobId, const JobState& jobState) noexcept = 0;
 };
 
 } // namespace vm_scheduler
--- libs/task_storage/include/task_storage_mock.h
+++ libs/task_storage/include/task_storage_mock.h
@@ -2,6 +2,8 @@
 
 #include "libs/task_storage/include/task_storage.h"
 
+#include <libs/state/include/task.h>
+
 #include <gmock/gmock.h>
 
 
@@ -11,18 +13,52 @@ class TaskStorageMock : public TaskStorage {
 public:
     using TaskStorage::TaskStorage;
 
-    MOCK_METHOD(Result<TaskId>, addTask, (const TaskParameters& taskParameters), (noexcept, override));
+    MOCK_METHOD(Result<CreatedJobs>, addTask, (const TaskParameters& taskParameters), (noexcept, override));
     MOCK_METHOD(Result<PlanId>, startScheduling,
                 (const std::string& backendId, const std::chrono::seconds& schedulingInterval), (noexcept, override));
     MOCK_METHOD(Result<State>, getCurrentState, (), (noexcept, override));
     MOCK_METHOD(Result<void>, commitPlanChange, (const StateChange& state, const PlanId planId), (noexcept, override));
     MOCK_METHOD(Result<std::vector<AllocationPendingVmInfo>>, getVmsToAllocate,
                 (const size_t maxVmAllocationCount), (noexcept , override));
-    MOCK_METHOD(Result<void>, setVmStatus, (const VmId id, const VmStatus status), (noexcept, override));
-    MOCK_METHOD(Result<void>, saveVmAllocationResult,
-                (const VmId id, const AllocatedVmInfo& allocatedVmInfo), (noexcept, override));
     MOCK_METHOD(Result<std::vector<TerminationPendingVmInfo>>, getVmsToTerminate,
                 (const size_t maxVmTerminationCount), (noexcept, override));
+    MOCK_METHOD(Result<void>, rollbackUnallocatedVmsState,
+                (const std::vector<VmId>& vmIds,
+                 const size_t vmRestartAttemptCount,
+                 const size_t jobRestartAttemptCount),
+                (noexcept, override));
+    MOCK_METHOD(Result<void>, returnUnterminatedVms, (const std::vector<VmId>& vmIds), (noexcept, override));
+    MOCK_METHOD(Result<void>, saveVmAllocationResult,
+                (const VmId id, const AllocatedVmInfo& allocatedVmInfo), (noexcept, override));
+    MOCK_METHOD(Result<void>, saveVmTerminationResult, (const VmId id), (noexcept, override));
+    MOCK_METHOD(Result<JobStates>, getJobStates, (const TaskId id), (noexcept, override));
+    MOCK_METHOD(Result<void>, cancelTask, (const TaskId taskId), (noexcept, override));
+    MOCK_METHOD(Result<void>, restartStaleAllocatingVms,
+                (const std::chrono::seconds& allocationTimeLimit,
+                 const size_t vmRestartAttemptCount),
+                (noexcept, override));
+    MOCK_METHOD(Result<void>, terminateStaleAllocatingVms,
+                (const std::chrono::seconds& allocationTimeLimit,
+                 const size_t vmRestartAttemptCount,
+                 const size_t jobRestartAttemptCount),
+                (noexcept, override));
+    MOCK_METHOD(Result<void>, terminateVmsWithInactiveAgents,
+                (const std::chrono::seconds& agentInactivityTimeLimit,
+                 const size_t jobRestartAttemptCount),
+                (noexcept, override));
+    MOCK_METHOD(Result<void>, terminateVmsWithoutAgents,
+                (const std::chrono::seconds& agentStartupTimeLimit,
+                 const size_t jobRestartAttemptCount),
+                (noexcept, override));
+    MOCK_METHOD(Result<std::vector<AssignedJob>>, getAssignedJobs,
+                (const VmId vmId),
+                (noexcept, override));
+    MOCK_METHOD(Result<JobToLaunch>, getJobToLaunch,
+                (const VmId vmId, const JobId jobId),
+                (noexcept, override));
+    MOCK_METHOD(Result<void>, updateJobState,
+                (const VmId vmId, const JobId jobId, const JobState& jobState),
+                (noexcept, override));
 };
 
 } // namespace vm_scheduler::testing
